bda2b031d8268828fd0f328742e02e60e6d395f4
deorfeal <88773323+deorfeal@users.noreply.github.com>
2024-02-05 09:18:23 +0200

    Add files via upload

---
+++ js/IkbmS5breHf7.js
@@ -0,0 +1,140 @@
+﻿var cbrTools = (function () {
+    function scrollToElementIfNeed($elt) {
+        var y = $elt.offset().top;
+        var space = 120;
+        y = y - space;
+        if (y < 0) y = 0;
+
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (!(scrollTop <= y && y < scrollTop + windowHeight - 2 * space)) {
+            if (scrollTop + windowHeight - 2 * space <= y && y < scrollTop + windowHeight) {
+                y = y + 2 * space - windowHeight;
+            }
+            window.scrollTo(0, y);
+        }
+    }
+    function scrollToElement($elt) {
+        var y = $elt.offset().top;
+        var eltHeight = $elt.height();
+        var windowHeight = $(window).height();
+        var scrollToY = eltHeight < windowHeight * 2 / 3 ? y - (windowHeight - eltHeight) / 2 : (y - windowHeight / 3);
+        if (scrollToY < 0) scrollToY = 0;
+
+        window.scrollTo(0, scrollToY);
+    }
+
+    var $liveRegion = null;
+    $(function () {
+        $liveRegion = $('<div/>', {
+            'role': 'status',
+            'aria-live': 'assertive',
+            'aria-relevant': 'additions'
+        })
+        .addClass('ui-helper-hidden-accessible')
+        .appendTo($('body'));
+    });
+    function accessibleMessage(message) {
+        if (message) {
+            $liveRegion.children().hide();
+            $('<div>').text(message).appendTo($liveRegion);
+        }
+    }
+
+    return {
+        scrollToElementIfNeed: scrollToElementIfNeed,
+        scrollToElement: scrollToElement,
+        accessibleMessage: accessibleMessage
+    }
+})();
+
+$(function () {
+    var $anchorElement;
+    function highlightAnchor() {
+        try {
+            $(window).unbind('hashchange.highlightAnchor');
+
+            var anchor = window.location.hash;
+            if (anchor && $anchorElement && $(anchor).get(0) === $anchorElement.get(0)) return;
+            if ($anchorElement) {
+                if ($anchorElement.is('.referenceable')) {
+                    $anchorElement.removeClass('yellow');
+                }
+                $anchorElement = null;
+            }
+            if (anchor && /^#[a-z0-9-_]+$/i.test(anchor)) {
+                var $elt = $(anchor);
+                if ($elt.length !== 0) {
+                    $anchorElement = $elt;
+                    if ($elt.is('.referenceable')) {
+                        $anchorElement.addClass('yellow');
+                    }
+                    $elt.trigger('open-anchor', [true]);
+                    if (anchor !== window.location.hash) {
+                        var from = window.location.hash;
+                        window.location.hash = anchor;
+                    }
+                }
+            }
+        }
+        finally {
+            $(window).bind('hashchange.highlightAnchor', highlightAnchor);
+        }
+    }
+    setTimeout(highlightAnchor, 0);
+
+    $.fn.replaceText = function (search, replace, text_only) {
+        return this.each(function () {
+            var node = this.firstChild,
+                val,
+                new_val,
+                remove = [];
+            if (node) {
+                do {
+                    if (node.nodeType === 3) {
+                        val = node.nodeValue;
+                        new_val = val.replace(search, replace);
+                        if (new_val !== val) {
+                            if (!text_only && /</.test(new_val)) {
+                                $(node).before(new_val);
+                                remove.push(node);
+                            } else {
+                                node.nodeValue = new_val;
+                            }
+                        }
+                    }
+                    else if (node.nodeType === 1) {
+                        $(node).replaceText(search, replace, text_only);
+                    }
+                } while (node = node.nextSibling);
+            }
+            remove.length && $(remove).remove();
+        });
+    };
+
+    function highlightSearch() {
+        try {
+            $(window).unbind('hashchange.highlightSearch');
+
+            if (location.hash && location.hash.indexOf('#highlight=') === 0) {
+                $('mark').each(function () { $(this).replaceWith($(this).html()); });
+                var wordsPattern = decodeURIComponent(location.hash.split('=')[1]);
+                if (/^[a-zа-яё0-9_\-]+(\|[a-zа-яё0-9_\-]+)*$/gi.exec(wordsPattern)) {
+                    $('#content').replaceText(new RegExp('(^|[^a-zа-яё0-9_\\-])(' + wordsPattern + ')(?=$|[^a-zа-яё0-9_\\-])', 'gi'), '$1<mark>$2</mark>');
+                }
+                var $marks = $('mark');
+                if ($marks.length) {
+                    $marks.trigger('open-anchor');
+                    //$marks.parents().addBack().each(function () { $(this).triggerHandler('open-anchor'); });
+                    setTimeout(function () { cbrTools.scrollToElementIfNeed($marks); }, 200);
+                }
+            }
+        }
+        finally {
+            $(window).bind('hashchange.highlightSearch', highlightSearch);
+        }
+    }
+    setTimeout(highlightSearch, 0);
+
+});
---
+++ js/ZlypZLEWguO0.js
@@ -0,0 +1,41 @@
+﻿Content_PageInfo = (function () {
+    function _init($qform) {
+        var $likeBtn = $qform.find('[data-helpful-btn=yes]');
+        var $dislikeBtn = $qform.find('[data-helpful-btn=no]');
+
+        $likeBtn.click(function () {
+            registerFeedback(true);
+        });
+
+        $dislikeBtn.click(function () {
+            registerFeedback(false);
+        });
+
+        function registerFeedback(isLike) {
+            $likeBtn.unbind('click');
+            $dislikeBtn.unbind('click');
+
+            $.ajax({
+                url: $qform.data('feedback-url'),
+                cache: false,
+                method: 'POST',
+                data: { isLike: isLike },
+                dataType: 'json'
+            });
+
+            var url = '/Content/PageInfo/RegisterFeedback?isLike=' + isLike;
+            var options = { title: 'Оценка полезности страницы', referer: window.location.href };
+            if (typeof (yaCounter5774506) !== 'undefined') {
+                yaCounter5774506.hit(url, options);
+            }
+            else if (typeof (ym) !== 'undefined') {
+                ym(5774506, 'hit', url, options);
+            }
+        }
+    }
+
+    return {
+        init: _init
+    };
+})();
+
